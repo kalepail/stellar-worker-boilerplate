22610fb1b693747a40100db641de7fb2a973bcd8
Tyler van der Hoeven <hi@tyvdh.com>
2024-01-04 11:21:44 -0500

    introduce bug with horizon

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-import { Keypair } from '@stellar/stellar-sdk'
+import { Horizon } from '@stellar/stellar-sdk'
+
+const horizon = new Horizon.Server('https://horizon-testnet.stellar.org')
 
 export interface Env {
 
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response(Keypair.random().publicKey());
+	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+		const account = await horizon.loadAccount('GAE2NBKSFTICKYNGXXCEWMOHZHEZPX23NWSCX5MBTVRNXL6RWHTETY3B')
+		return account
 	},
 };
